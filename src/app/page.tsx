183e61a549df4eb67a5de03782fdd8ba8108c213
Ruy Junior <ruyjunior21@gmail.com>
2025-03-12 11:49:33 -0300

    First commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing/Pricing";
+//import Pricing from "@/components/Pricing/Pricing";
 import FAQ from "@/components/FAQ";
-import Benefits from "@/components/Benefits/Benefits";
+//import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
 import Location from "@/components/Location";
 import Team from "@/components/Team";
-import Payments from "@/components/Payments";
+//import Payments from "@/components/Payments";
 //import NewsCarousel from "@/components/News";
 
 const HomePage: React.FC = () => {
